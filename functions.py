06dc69d30917931052f2f998b6691b9146c08f2e
Jiten Sardana <jitensardana@Jitens-MacBook-Pro.local>
2017-10-04 19:35:01 +0530

    Major changes. Output still not okay. Write draw_lines function on your own

--- functions.py
+++ functions.py
@@ -3,176 +3,329 @@ import numpy as np
 import math
 
 
-# Converts color of the image from RGB to GRAY
 def grayscale(img):
+    """Applies the Grayscale transform
+    This will return an image with only one color channel
+    but NOTE: to see the returned image as grayscale
+    you should call plt.imshow(gray, cmap='gray')"""
     return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-def moving_average(avg, new_sample, N=20):
-    if avg == 0:
-        return new_sample
-    avg -= avg/N
-    avg += new_sample/N
-    return avg
 
-
-# Find edges using canny detection algorithm
 def canny(img, low_threshold, high_threshold):
+    """Applies the Canny transform"""
     return cv2.Canny(img, low_threshold, high_threshold)
 
 
-# Smoothing using Gaussian Blur
 def gaussian_blur(img, kernel_size):
+    """Applies a Gaussian Noise kernel"""
     return cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
 
 
-# Define the region of interest
 def region_of_interest(img, vertices):
-    # make a numpy array similar to shape and size as that of image and filled with 0
+    """
+    Applies an image mask.
+
+    Only keeps the region of the image defined by the polygon
+    formed from `vertices`. The rest of the image is set to black.
+    """
+    # defining a blank mask to start with
     mask = np.zeros_like(img)
 
-    if len(img.shape) > 2 :
+    # defining a 3 channel or 1 channel color to fill the mask with depending on the input image
+    if len(img.shape) > 2:
         channel_count = img.shape[2]
-        ignore_mask_color = (255,)*channel_count
+        print (channel_count)  # i.e. 3 or 4 depending on your image
+        ignore_mask_color = (255,) * channel_count
     else:
         ignore_mask_color = 255
 
+    # filling pixels inside the polygon defined by "vertices" with the fill color
     cv2.fillPoly(mask, vertices, ignore_mask_color)
 
-    masked_image = cv2.bitwise_and(mask, img)
-
+    # returning the image only where mask pixels are nonzero
+    masked_image = cv2.bitwise_and(img, mask)
     return masked_image
 
 
-# Returns the original image with hough lines drawn on it
-def weighted_img(img, initial_img, alpha = 0.8, beta = 1., gamma=0.):
-    '''
-        `img` is the output of the hough_lines(), An image with lines drawn on it.
-        Should be a blank image (all black) with lines drawn on it.
+def draw_lines(img, lines, color=[255, 0, 0], thickness=2):
+    """
+    NOTE: this is the function you might want to use as a starting point once you want to
+    average/extrapolate the line segments you detect to map out the full
+    extent of the lane (going from the result shown in raw-lines-example.mp4
+    to that shown in P1_example.mp4).
 
-        `initial_img` should be the image before any processing.
+    Think about things like separating line segments by their
+    slope ((y2-y1)/(x2-x1)) to decide which segments are part of the left
+    line vs. the right line.  Then, you can average the position of each of
+    the lines and extrapolate to the top and bottom of the lane.
 
-        The result image is computed as follows:
-
-        initial_img * alpha + img * beta + gamma
-        NOTE: initial_img and img must be the same shape!
-    '''
-    return cv2.addWeighted(initial_img, alpha, img, beta, gamma)
-
-
-# Draw lane lines using hough transform on the canny image
-def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
-    lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]), minLineLength=min_line_len, maxLineGap=max_line_gap)
-
-    line_img = np.zeros(img.shape, dtype=np.uint8)
-    draw_lines(line_img, lines)
-    return line_img
+    This function draws `lines` with `color` and `thickness`.
+    Lines are drawn on the image inplace (mutates the image).
+    If you want to make the lines semi-transparent, think about combining
+    this function with the weighted_img() function below
+    """
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            cv2.line(img, (x1, y1), (x2, y2), color, thickness)
 
 
-def find_line_fit(slope_intercept):
-    kept_slopes = []
-    kept_intercepts = []
+# Python 3 has support for cool math symbols.
 
-    print("Slope & intercept : ", slope_intercept)
+def weighted_img(img, initial_img, a=0.8, b=1., c=0.):
+    """
+    `img` is the output of the hough_lines(), An image with lines drawn on it.
+    Should be a blank image (all black) with lines drawn on it.
 
-    if len(slope_intercept) == 1 :
-        return slope_intercept[0][0], slope_intercept[0][1]
+    `initial_img` should be the image before any processing.
 
-    slopes = [pair[0] for pair in slope_intercept]
-    mean_slope = np.mean(slopes)
-    std_slope = np.std(slopes)
+    The result image is computed as follows:
 
-    for pair in slope_intercept:
-        slope = pair[0]
-        if slope - mean_slope < 1.5 * std_slope:
-            kept_slopes.append(slope)
-            kept_intercepts.append(pair[1])
-
-    if not kept_slopes:
-        kept_slopes = slopes
-        kept_intercepts = [pair[1] for pair in slope_intercept]
-
-    slope = np.mean(kept_slopes)
-    intercept = np.mean(kept_intercepts)
-    print("Slope: ", slope, "Intercept: ", intercept)
-    return slope, intercept
+    initial_img * a + img * b + c
+    NOTE: initial_img and img must be the same shape!
+    """
+    return cv2.addWeighted(initial_img, a, img, b, c)
 
 
-def intersection_x(coef1, intercept1, coef2, intercept2):
-    x = (intercept2 - intercept1)/(coef1 - coef2)
-    return x
+# seventh attempt, based on second attempt, second take
+#   This version of draw_lines averages out all of the left and right lane lines
+#   And just draw a single line for both
+#
+#   This attempt seems to work for case 1 and 2, most of the Optional Challenge! :)
+#   It appears that if we add a yellow_white color filter using inRange and a range of
+#   rho larger than 1 with higher thresholds (higher minimum votes) to eliminate extra lines.
+def my_draw_lines2(img, lines, color=[255, 0, 0], thickness=6, debug=False):
+    ysize = img.shape[0]
+    try:
+        # rightline and leftline cumlators
+        rl = {'num': 0, 'slope': 0.0, 'x1': 0, 'y1': 0, 'x2': 0, 'y2': 0}
+        ll = {'num': 0, 'slope': 0.0, 'x1': 0, 'y1': 0, 'x2': 0, 'y2': 0}
+        for line in lines:
+            for x1, y1, x2, y2 in line:
+                slope = ((y2 - y1) / (x2 - x1))
+                if slope > 0.5 and slope < 1.0:  # right
+                    rl['num'] += 1
+                    rl['slope'] += slope
+                    rl['x1'] += x1
+                    rl['y1'] += y1
+                    rl['x2'] += x2
+                    rl['y2'] += y2
+                elif slope > -1.0 and slope < -0.5:  # left
+                    ll['num'] += 1
+                    ll['slope'] += slope
+                    ll['x1'] += x1
+                    ll['y1'] += y1
+                    ll['x2'] += x2
+                    ll['y2'] += y2
+
+        if rl['num'] > 0 and ll['num'] > 0:
+            # average/extrapolate all of the lines that makes the right line
+            rslope = rl['slope'] / rl['num']
+            rx1 = int(rl['x1'] / rl['num'])
+            ry1 = int(rl['y1'] / rl['num'])
+            rx2 = int(rl['x2'] / rl['num'])
+            ry2 = int(rl['y2'] / rl['num'])
+
+            # average/extrapolate all of the lines that makes the left line
+            lslope = ll['slope'] / ll['num']
+            lx1 = int(ll['x1'] / ll['num'])
+            ly1 = int(ll['y1'] / ll['num'])
+            lx2 = int(ll['x2'] / ll['num'])
+            ly2 = int(ll['y2'] / ll['num'])
+
+            # find the right and left line's intercept, which means solve the following two equations
+            # rslope = ( yi - ry1 )/( xi - rx1)
+            # lslope = ( yi = ly1 )/( xi - lx1)
+            # solve for (xi, yi): the intercept of the left and right lines
+            # which is:  xi = (ly2 - ry2 + rslope*rx2 - lslope*lx2)/(rslope-lslope)
+            # and        yi = ry2 + rslope*(xi-rx2)
+            xi = int((ly2 - ry2 + rslope * rx2 - lslope * lx2) / (rslope - lslope))
+            yi = int(ry2 + rslope * (xi - rx2))
+
+            # calculate backoff from intercept for right line
+            if rslope > 0.5 and rslope < 1.0:  # right
+                ry1 = yi + thickness * 3
+                rx1 = int(rx2 - (ry2 - ry1) / rslope)
+                ry2 = ysize - 1
+                rx2 = int(rx1 + (ry2 - ry1) / rslope)
+                cv2.line(img, (rx1, ry1), (rx2, ry2), [255, 0, 0], thickness)
+
+            # calculate backoff from intercept for left line
+            if lslope < -0.5 and lslope > -1.0:  # left
+                ly1 = yi + thickness * 3
+                lx1 = int(lx2 - (ly2 - ly1) / lslope)
+                ly2 = ysize - 1
+                lx2 = int(lx1 + (ly2 - ly1) / lslope)
+                cv2.line(img, (lx1, ly1), (lx2, ly2), [255, 0, 0], thickness)
+
+        return lslope + rslope, lslope, rslope, lx1, rx1
+    except:
+        return -1000, 0.0, 0.0, 0.0, 0, 0
 
 
-def draw_linear_regression_line(coef, intercept, intersection_x, img , imshape = [540,960], color = [255,0,0], thickness = 2):
-    print("Coef: ", coef, " Intercept: ", intercept, " intersection_x: ", intersection_x)
+def my_hough_lines2(img, rho, theta, threshold, min_line_len, max_line_gap, debug=False):
+    """
+    `img` should be the output of a Canny transform.
 
-    point_one = (int(intersection_x), int(intersection_x * coef + intercept))
-    if coef > 0:
-        point_two = (imshape[1], int(imshape[1] * coef + intercept))
-    elif coef < 0:
-        point_two = (0, int(0 * coef + intercept))
+    Returns an image with hough lines drawn using the new single line for left and right lane line method.
+    """
+    lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]), minLineLength=min_line_len,
+                            maxLineGap=max_line_gap)
+    masked_lines = np.zeros(img.shape, dtype=np.uint8)
+    lane_info = my_draw_lines2(masked_lines, lines, debug=debug)
 
-    print("Point one: ", point_one, "Point two: ", point_two)
+    # Create a "color" binary image to combine with original image
+    ignore_color = np.copy(masked_lines) * 0  # creating a blank color channel for combining
+    line_image = np.dstack((masked_lines, ignore_color, ignore_color))
 
-    cv2.line(img, point_one, point_two, color, thickness)
+    return line_image, lane_info
 
 
-# This function draws the lines by taking the coordinates from the Hough transform function in opencv
-def draw_lines(img, lines, color=[255,0,0], thickness = 2):
-    positive_slope_points = []
-    negative_slope_points = []
-    positive_slope_intercept = []
-    negative_slope_intercept = []
+def my_draw_masked_area(img, areas, color=[128, 0, 128], thickness=2):
+    for points in areas:
+        for i in range(len(points) - 1):
+            cv2.line(img, (points[i][0], points[i][1]), (points[i + 1][0], points[i + 1][1]), color, thickness)
+        cv2.line(img, (points[0][0], points[0][1]), (points[len(points) - 1][0], points[len(points) - 1][1]), color,
+                 thickness)
+
+
+def my_image_only_yellow_white_curve1(image):
+    # setup inRange to mask off everything except white and yellow
+    lower_yellow_white = np.array([192, 192, 32])
+    upper_yellow_white = np.array([255, 255, 255])
+    mask = cv2.inRange(image, lower_yellow_white, upper_yellow_white)
+    return cv2.bitwise_and(image, image, mask=mask)
+
+
+def my_image_only_yellow_white_curve2(image):
+    # setup inRange to mask off everything except white and yellow
+    lower_yellow_white = np.array([140, 140, 64])
+    upper_yellow_white = np.array([255, 255, 255])
+    mask = cv2.inRange(image, lower_yellow_white, upper_yellow_white)
+    return cv2.bitwise_and(image, image, mask=mask)
+
+
+def perp(a):
+    b = np.empty_like(a)
+    b[0] = -a[1]
+    b[1] = a[0]
+    return b
+
+
+# line segment a given by endpoints a1, a2
+# line segment b given by endpoints b1, b2
+# return
+def seg_intersect(a1, a2, b1, b2):
+    da = a2 - a1
+    db = b2 - b1
+    dp = a1 - b1
+    dap = perp(da)
+    denom = np.dot(dap, db)
+    num = np.dot(dap, dp)
+    return (num / denom.astype(float)) * db + b1
+
+
+def movingAverage(avg, new_sample, N=20):
+    if (avg == 0):
+        return new_sample
+    avg -= avg / N;
+    avg += new_sample / N;
+    return avg;
+
+
+def draw_lines_orig(img, lines, color=[255, 0, 0], thickness=2):
+    largestLeftLineSize = 0
+    largestRightLineSize = 0
+    largestLeftLine = (0, 0, 0, 0)
+    largestRightLine = (0, 0, 0, 0)
+
+    if lines is None:
+        avgx1, avgy1, avgx2, avgy2 = avgLeft
+        cv2.line(img, (int(avgx1), int(avgy1)), (int(avgx2), int(avgy2)), [255, 255, 255], 12)  # draw left line
+        avgx1, avgy1, avgx2, avgy2 = avgRight
+        cv2.line(img, (int(avgx1), int(avgy1)), (int(avgx2), int(avgy2)), [255, 255, 255], 12)  # draw right line
+        return
 
     for line in lines:
-        for x1,y1,x2,y2 in line:
-            slope = (y2-y1)/(x2-x1)
-            length = math.sqrt((x2-x1)**2 + (y2-y1)**2)
-            if not math.isnan(slope):
-                if length > 50:
-                    if slope > 0:
-                        positive_slope_points.append([x1, y1])
-                        positive_slope_points.append([x2, y2])
-                        positive_slope_intercept.append([slope, y1 - slope * x1])
+        for x1, y1, x2, y2 in line:
+            size = math.hypot(x2 - x1, y2 - y1)
+            slope = ((y2 - y1) / (x2 - x1))
+            # Filter slope based on incline and
+            # find the most dominent segment based on length
+            if (slope > 0.5):  # right
+                if (size > largestRightLineSize):
+                    largestRightLine = (x1, y1, x2, y2)
+                cv2.line(img, (x1, y1), (x2, y2), color, thickness)
+            elif (slope < -0.5):  # left
+                if (size > largestLeftLineSize):
+                    largestLeftLine = (x1, y1, x2, y2)
+                cv2.line(img, (x1, y1), (x2, y2), color, thickness)
 
-                    elif slope < 0:
-                        negative_slope_points.append([x1, y1])
-                        negative_slope_points.append([x2, y2])
-                        negative_slope_intercept.append([slope, y1 - slope * x1])
+    # Define an imaginary horizontal line in the center of the screen
+    # and at the bottom of the image, to extrapolate determined segment
+    imgHeight, imgWidth = (img.shape[0], img.shape[1])
+    upLinePoint1 = np.array([0, int(imgHeight - (imgHeight / 3))])
+    upLinePoint2 = np.array([int(imgWidth), int(imgHeight - (imgHeight / 3))])
+    downLinePoint1 = np.array([0, int(imgHeight)])
+    downLinePoint2 = np.array([int(imgWidth), int(imgHeight)])
+
+    # Find the intersection of dominant lane with an imaginary horizontal line
+    # in the middle of the image and at the bottom of the image.
+    p3 = np.array([largestLeftLine[0], largestLeftLine[1]])
+    p4 = np.array([largestLeftLine[2], largestLeftLine[3]])
+    upLeftPoint = seg_intersect(upLinePoint1, upLinePoint2, p3, p4)
+    downLeftPoint = seg_intersect(downLinePoint1, downLinePoint2, p3, p4)
+    if (math.isnan(upLeftPoint[0]) or math.isnan(downLeftPoint[0])):
+        avgx1, avgy1, avgx2, avgy2 = avgLeft
+        cv2.line(img, (int(avgx1), int(avgy1)), (int(avgx2), int(avgy2)), [255, 255, 255], 12)  # draw left line
+        avgx1, avgy1, avgx2, avgy2 = avgRight
+        cv2.line(img, (int(avgx1), int(avgy1)), (int(avgx2), int(avgy2)), [255, 255, 255], 12)  # draw right line
+        return
+    cv2.line(img, (int(upLeftPoint[0]), int(upLeftPoint[1])), (int(downLeftPoint[0]), int(downLeftPoint[1])),
+             [0, 0, 255], 8)  # draw left line
+
+    # Calculate the average position of detected left lane over multiple video frames and draw
+    global avgLeft
+    avgx1, avgy1, avgx2, avgy2 = avgLeft
+    avgLeft = (
+        movingAverage(avgx1, upLeftPoint[0]), movingAverage(avgy1, upLeftPoint[1]),
+        movingAverage(avgx2, downLeftPoint[0]),
+        movingAverage(avgy2, downLeftPoint[1]))
+    avgx1, avgy1, avgx2, avgy2 = avgLeft
+    cv2.line(img, (int(avgx1), int(avgy1)), (int(avgx2), int(avgy2)), [255, 255, 255], 12)  # draw left line
+
+    # Find the intersection of dominant lane with an imaginary horizontal line
+    # in the middle of the image and at the bottom of the image.
+    p5 = np.array([largestRightLine[0], largestRightLine[1]])
+    p6 = np.array([largestRightLine[2], largestRightLine[3]])
+    upRightPoint = seg_intersect(upLinePoint1, upLinePoint2, p5, p6)
+    downRightPoint = seg_intersect(downLinePoint1, downLinePoint2, p5, p6)
+    if (math.isnan(upRightPoint[0]) or math.isnan(downRightPoint[0])):
+        avgx1, avgy1, avgx2, avgy2 = avgLeft
+        cv2.line(img, (int(avgx1), int(avgy1)), (int(avgx2), int(avgy2)), [255, 255, 255], 12)  # draw left line
+        avgx1, avgy1, avgx2, avgy2 = avgRight
+        cv2.line(img, (int(avgx1), int(avgy1)), (int(avgx2), int(avgy2)), [255, 255, 255], 12)  # draw right line
+        return
+    cv2.line(img, (int(upRightPoint[0]), int(upRightPoint[1])), (int(downRightPoint[0]), int(downRightPoint[1])),
+             [0, 0, 255], 8)  # draw left line
+
+    # Calculate the average position of detected right lane over multiple video frames and draw
+    global avgRight
+    avgx1, avgy1, avgx2, avgy2 = avgRight
+    avgRight = (movingAverage(avgx1, upRightPoint[0]), movingAverage(avgy1, upRightPoint[1]),
+                movingAverage(avgx2, downRightPoint[0]), movingAverage(avgy2, downRightPoint[1]))
+    avgx1, avgy1, avgx2, avgy2 = avgRight
+    cv2.line(img, (int(avgx1), int(avgy1)), (int(avgx2), int(avgy2)), [255, 255, 255], 12)  # draw left line
 
 
-    # If either array is empty waive the length requirement
+def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
+    """
+    `img` should be the output of a Canny transform.
 
-    if not positive_slope_points:
-        for line in lines:
-            for x1,y1,x2,y2 in line:
-                slope = (y1-y2)/(x1-x2)
-                if slope > 0:
-                    positive_slope_points.append([x1,y1])
-                    positive_slope_points.append([x2,y2])
-                    positive_slope_intercept.append([slope, y1 - slope * x1])
-
-    if not negative_slope_points:
-        for line in lines:
-            for x1,y1,x2,y2 in line:
-                slope = (y1-y2)/(x1-x2)
-                if slope < 0:
-                    negative_slope_points.append([x1,y1])
-                    negative_slope_points.append([x2,y2])
-                    negative_slope_intercept.append([slope, y1 - slope * x1])
-
-    if not positive_slope_points:
-        print("positive_slope_points still empty")
-    if not negative_slope_points:
-        print("negative_slope_points still empty")
-
-    # For debugging
-    positive_slope_points = np.array(positive_slope_points)
-    negative_slope_points = np.array(negative_slope_points)
-
-    pos_coef, pos_intercept = find_line_fit(positive_slope_intercept)
-    neg_coef, neg_intercept = find_line_fit(negative_slope_intercept)
-
-    intersection_x_coord = intersection_x(pos_coef, pos_intercept, neg_coef, neg_intercept)
-
-    draw_linear_regression_line(pos_coef, pos_intercept, intersection_x_coord, img, color=color, thickness=thickness)
-    draw_linear_regression_line(neg_coef, neg_intercept, intersection_x_coord, img, color=color, thickness=thickness)
+    Returns an image with hough lines drawn.
+    """
+    lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]), minLineLength=min_line_len,
+                            maxLineGap=max_line_gap)
+    #print img.shape
+    line_img = np.zeros(img.shape+(3,), dtype=np.uint8)
+    draw_lines(line_img, lines)
+    return line_img
--- lane_detection.py
+++ lane_detection.py
@@ -1,50 +1,136 @@
-import numpy as np
 import cv2
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-import functions
+from functions import grayscale, gaussian_blur, canny, my_hough_lines2, my_image_only_yellow_white_curve2, my_image_only_yellow_white_curve1, weighted_img,\
+    region_of_interest, hough_lines
 import sys
 
-def draw_lane_lines(image = sys.argv[1]):
-    image = mpimg.imread(image)
+
+def procimage2(image):
+    # mask calculations
     imshape = image.shape
-    grayscaled_img = functions.grayscale(image)
-    plt.subplot(2,2,1)
-    plt.imshow(grayscaled_img, cmap="gray")
+    xbottom1 = int(imshape[1] / 16)
+    xbottom2 = int(imshape[1] * 15 / 16)
+    xtop1 = int(imshape[1] * 14 / 32)
+    xtop2 = int(imshape[1] * 18 / 32)
+    ybottom1 = imshape[0]
+    ybottom2 = imshape[0]
+    ytopbox = int(imshape[0] * 9 / 16)
 
-    # Smoothing
-    blurred_img = functions.gaussian_blur(grayscaled_img, 7)
+    # Read in the image and convert to grayscale
+    gray = grayscale(my_image_only_yellow_white_curve1(image))
 
-    # Edge Detection
-    edges_img = functions.canny(blurred_img, 50, 150)
+    # Define a kernel size for Gaussian smoothing / blurring
+    kernel_size = 7  # Must be an odd number (3, 5, 7...)
+    blur_gray = gaussian_blur(gray, kernel_size)
 
-    # Mask edges img
+    # Define our parameters for Canny and run it
+    low_threshold = 5
+    high_threshold = 17
+    edges = canny(blur_gray, low_threshold, high_threshold)
+    #cv2.imshow("edges",edges)
+    #cv2.waitKey()
+    #print(edges.shape)
 
-    vertices = np.array([[(0, imshape[0]), (465, 320), (475, 320), (imshape[1], imshape[0])]], dtype = np.int32)
-    edges_img_with_mask = functions.region_of_interest(edges_img, vertices)
+    # This time we are defining a four sided polygon to mask
+    # We can lift the mask higher now, since the line drawing function is a bit smarter
+    vertices = np.array([[(xbottom1, ybottom1), (xtop1, ytopbox), (xtop2, ytopbox), (xbottom2, ybottom2)]],
+                        dtype=np.int32)
+    masked_edges = region_of_interest(edges, vertices)
+    #cv2.imshow("masked", masked_edges)
+    #cv2.waitKey()
 
-    plt.subplot(2,2,2)
-    plt.imshow(edges_img_with_mask)
+    # Define the Hough transform parameters
+    # Make a blank the same size as our image to draw on
+    rho = 2  # distance resolution in pixels of the Hough grid
+    theta = np.pi / 180  # angular resolution in radians of the Hough grid
+    threshold = 40  # minimum number of votes (intersections in Hough grid cell)
+    min_line_length = 75  # minimum number of pixels making up a line
+    max_line_gap = 50  # maximum gap in pixels between connectable line segments
 
-    rho = 2
-    theta = np.pi/180
-    threshold = 45
-    min_line_len = 40
-    max_line_gap = 100
-    lines_img = functions.hough_lines(edges_img_with_mask, rho, theta, threshold, min_line_len, max_line_gap)
+    # Run Hough on edge detected image
+    # Output "masked_lines" is a single channel mask
+    ignore_color = np.zeros(masked_edges.shape, dtype=np.uint8)
+    original_image = np.dstack((masked_edges, ignore_color, ignore_color))
 
-    hough_rgb_img = cv2.cvtColor(lines_img, cv2.COLOR_GRAY2BGR)
-    plt.subplot(2,2,3)
-    plt.imshow(hough_rgb_img)
-    final_img = functions.weighted_img(hough_rgb_img, image)
+    line_image = hough_lines(masked_edges, rho, theta, threshold, min_line_length, max_line_gap)
 
-    plt.subplot(2,2,4)
-    plt.imshow(final_img)
+    # if first attempted failed - that means we are dealing with yellow and white on white.
+    #   try second mask
+    # if np.array_equal(line_image, original_image):
+    """
+    if lane_info[0] == -1000:
+        # Read in the image and convert to grayscale
+        gray = grayscale(my_image_only_yellow_white_curve2(image))
 
-    return final_img
+        # Define a kernel size for Gaussian smoothing / blurring
+        kernel_size = 13  # Must be an odd number (3, 5, 7...)
+        blur_gray = gaussian_blur(gray, kernel_size)
+
+        # Define our parameters for Canny and run it
+        low_threshold = 10
+        high_threshold = 36
+        edges = canny(blur_gray, low_threshold, high_threshold)
+
+        # This time we are defining a four sided polygon to mask
+        # We can lift the mask higher now, since the line drawing function is a bit smarter
+        masked_edges = region_of_interest(edges, vertices)
+        plt.imshow(masked_edges)
+        #plt.show()
+
+        # Define the Hough transform parameters
+        # Make a blank the same size as our image to draw on
+        rho = 16  # distance resolution in pixels of the Hough grid
+        theta = np.pi / 180  # angular resolution in radians of the Hough grid
+        threshold = 130  # minimum number of votes (intersections in Hough grid cell)
+        min_line_length = 75  # minimum number of pixels making up a line
+        max_line_gap = 100  # maximum gap in pixels between connectable line segments
+
+        # debugging
+        # orignial_image = np.dstack((masked_edges, ignore_color, ignore_color))
+
+        # Run Hough on edge detected image
+        # Output "masked_lines" is a single channel mask
+
+        line_image, lane_info = my_hough_lines2(masked_edges, rho, theta, threshold, min_line_length, max_line_gap)
+        #cv2.imshow("line image", line_image)
+        #cv2.waitKey()
+
+        # debugging short
+        # return weighted_img(line_image, original_image)
+    """
+    # my_draw_masked_area(line_image, vertices)
+    # line_image = weighted_img(line_image, original_image)
+    # return line_image
+
+    # return the merged red colored lines with the image as background
+    return weighted_img(line_image, image)
 
 
-if __name__ == "__main__":
-    draw_lane_lines()
+# loop through and display all of the process images
+import os
+import re
 
+filepath = "test_images/"
+pattern = re.compile("^.+processed.jpg$")
+files = os.listdir(filepath)
+for file in files:
+    # import the image if it is not a saved output
+    if not pattern.match(file):
+        image_filepath = filepath + file
+        image = mpimg.imread(image_filepath)
 
+        # process image
+        image_wlines = procimage2(image)  # attempt 2
+        print('Image ', image_filepath, ' has dimensions: ', image.shape)
+
+        # next image
+        plt.figure()
+
+        # plot the image
+        plt.imshow(image_wlines)
+        #plt.show()
+
+        # writeout the image with "-processed" in the name so it will not be reprocessed.
+        plt.savefig(image_filepath.replace(".jpg", "-processed.jpg"))
